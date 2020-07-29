b1b94fc16ff7b5adc483bf852f21e4d1698f0658
Srinath.M <44088670+mugundhs29@users.noreply.github.com>
2020-07-29 12:07:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 28 23:14:19 2020
+
+@author: srinath.mugundhan
+"""
+import numpy as np
+import pandas as pd
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+import pickle
+import joblib
+import traceback
+
+# Your API definition
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if lr:
+        try:
+            json_ = request.json
+            print(json_)
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(lr.predict_proba(query))
+
+            return jsonify({'prediction': str(prediction)})
+
+        except:
+            return jsonify({'trace': traceback.format_exc()})
+    else:
+        print ('Train the model first')
+        return ('No model here to use')
+
+if __name__ == '__main__':
+    try:
+        port = int(sys.argv[1]) # This is for a command-line input
+    except:
+        port = 5000 # If you don't provide any port the port will be set to 12345
+
+    lr = joblib.load("model_LR.pkl") # Load "model.pkl"
+    print ('Model loaded')
+    model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
+    print ('Model columns loaded')
+
+    app.run(port=port, debug=False)
